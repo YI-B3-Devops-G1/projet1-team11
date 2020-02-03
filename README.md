e4b07550adea80c23456166a682b07db1c2d12b6
Voltzy9 <arthur.genthial@ynov.com>
2020-02-03 14:19:45 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# B3 Devops - Projet 1
+## Info
+Team 11
+---
+mail: arthur.genthial@ynov.com
+github_username: Voltzy9
+---
+[...]
+## DockerHub
+link: https://...
---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var express = require('express');
+var port = 3000;
+var app = express();
+
+
+
+app.get('/',(req,res) => {
+    res.json({message : "leo le bg de la night", method : req.method});
+});
+app.get('/status', (req,res) => {
+    res.json({
+        status:"ok",
+        postgresUptime:String,
+        redisConnectClient:Number
+    })
+});
+
+app.listen(port);
+
+console.log('Running on port :'+ port );
